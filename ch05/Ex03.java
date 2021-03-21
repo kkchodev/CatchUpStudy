49ce12a98c4f988978ac56bfe38933f05efb108f
kkchodev <ssys6565@gmail.com>
2021-03-21 22:53:35 +0900

    자바의 정석 ch05.배열

--- ch05/Ex03.java
+++ ch05/Ex03.java
@@ -13,11 +13,12 @@ public class Ex03{
 		
 		System.out.println("[변경 전]");
 		System.out.println("arr.length: "+arr.length);
+		
 		for(int i=0; i<arr.length;i++) {
 			System.out.println("arr["+i+"]: "+arr[i]);
 		}
 		
-		int[] tmp = new int[arr.length*2]; // 배열 arr의 길이보다 2배 긴 배열 tmp 선언
+		int[] tmp = new int[arr.length*2]; // 배열 arr의 길이보다 2배
 		// 배열 arr에 저장된 값들을 배열 tmp에 복사
 		for(int i=0; i<arr.length; i++) {
 			tmp[i] = arr[i];
@@ -29,5 +30,6 @@ public class Ex03{
 		for(int i=0; i<arr.length;i++) {
 			System.out.println("arr["+i+"]: "+arr[i]);
 		}
+		//근데 배열 길이만 늘어났지 값은 배열 1~5에만 있으니까..
 	}
 }
